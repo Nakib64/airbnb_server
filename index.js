46f351ed70782898936125eab9d3cc8434dbd548
Nakib64 <nafiz2282@gmail.com>
2025-09-13 00:39:33 +0600

    testing

--- index.js
+++ index.js
@@ -43,158 +43,158 @@ async function connectDB() {
 		services_trans_en = await db.collection("services_trans_en");
 		services_trans_bn = await db.collection("services_trans_bn");
 		console.log("✅ Connected to MongoDB");
+
+		// Test route
+		app.get("/", (req, res) => {
+			res.send("Server is running");
+		});
+
+		// Get hotels by division and language
+		app.get("/hotels", async (req, res) => {
+			try {
+				const { division, language } = req.query;
+
+				if (!division || !language) {
+					return res
+						.status(400)
+						.json({ message: "division and language are required" });
+				}
+
+				// 1. Get listings for   the division
+				const listings = await hotels.find({ division }).limit(8).toArray();
+
+				const hotelsId = listings.map((l) => l._id);
+
+				// 2. Get translations for those hotels
+				let translations;
+				if (language == "en") {
+					translations = await hotel_trans_en
+						.find({ listingId: { $in: hotelsId } })
+						.toArray();
+				} else if (language == "bn") {
+					translations = await hotel_trans_bn
+						.find({ listingId: { $in: hotelsId } })
+						.toArray();
+				}
+
+				// 3. Merge hotels with translations
+				const merged = listings.map((listing) => {
+					const translation = translations.find((t) => t.listingId === listing._id);
+					return {
+						...listing,
+						title: translation ? translation.title : listing.title,
+						description: translation ? translation.description : listing.description,
+					};
+				});
+				res.json(merged);
+			} catch (err) {
+				console.error(err);
+				res.status(500).json({ message: "Internal server error" });
+			}
+		});
+
+		//data for experience page
+
+		app.get("/services", async (req, res) => {
+			try {
+				const { category, language, limit = 8 } = req.query;
+
+				if (!category || !language) {
+					return res
+						.status(400)
+						.json({ message: "division and language are required" });
+				}
+
+				// 1. Get listings for   the division
+				const listings = await services.find({ category }).toArray();
+				const servicesId = listings.map((l) => l._id);
+
+				// 2. Get translations for those hotels
+				let translations;
+				if (language == "en") {
+					translations = await services_trans_en
+						.find({ listingId: { $in: servicesId } })
+						.toArray();
+				} else if (language == "bn") {
+					translations = await services_trans_bn
+						.find({ listingId: { $in: servicesId } })
+						.toArray();
+				}
+
+				// 3. Merge hotels with translations
+				const merged = listings.map((listing) => {
+					const translation = translations.find((t) => t.listingId === listing._id);
+					return {
+						...listing,
+						title: translation ? translation.title : listing.title,
+						description: translation ? translation.description : listing.description,
+					};
+				});
+
+				// 4. Shuffle merged array
+				for (let i = merged.length - 1; i > 0; i--) {
+					const j = Math.floor(Math.random() * (i + 1));
+					[merged[i], merged[j]] = [merged[j], merged[i]];
+				}
+				res.json(merged);
+			} catch (err) {
+				console.error(err);
+				res.status(500).json({ message: "Internal server error" });
+			}
+		});
+		app.get("/experiences", async (req, res) => {
+			try {
+				const { division, language, limit = 8 } = req.query;
+
+				if (!division || !language) {
+					return res
+						.status(400)
+						.json({ message: "division and language are required" });
+				}
+
+				// 1. Get listings for   the division
+				const listings = await experiences.find({ division }).toArray();
+
+				const experiencesId = listings.map((l) => l._id);
+
+				// 2. Get translations for those hotels
+				let translations;
+				if (language == "en") {
+					translations = await experiences_trans_en
+						.find({ listingId: { $in: experiencesId } })
+						.toArray();
+				} else if (language == "bn") {
+					translations = await experiences_trans_bn
+						.find({ listingId: { $in: experiencesId } })
+						.toArray();
+				}
+
+				// 3. Merge hotels with translations
+				const merged = listings.map((listing) => {
+					const translation = translations.find((t) => t.listingId === listing._id);
+					return {
+						...listing,
+						title: translation ? translation.title : listing.title,
+						description: translation ? translation.description : listing.description,
+					};
+				});
+
+				// 4. Shuffle merged array
+				for (let i = merged.length - 1; i > 0; i--) {
+					const j = Math.floor(Math.random() * (i + 1));
+					[merged[i], merged[j]] = [merged[j], merged[i]];
+				}
+				res.json(merged);
+			} catch (err) {
+				console.error(err);
+				res.status(500).json({ message: "Internal server error" });
+			}
+		});
 	} catch (err) {
 		console.error("❌ MongoDB connection error:", err);
 	}
 }
 connectDB();
-
-// Test route
-app.get("/", (req, res) => {
-	res.send("Server is running");
-});
-
-// Get hotels by division and language
-app.get("/hotels", async (req, res) => {
-	try {
-		const { division, language } = req.query;
-
-		if (!division || !language) {
-			return res
-				.status(400)
-				.json({ message: "division and language are required" });
-		}
-
-		// 1. Get listings for   the division
-		const listings = await hotels.find({ division }).limit(8).toArray();
-
-		const hotelsId = listings.map((l) => l._id);
-
-		// 2. Get translations for those hotels
-		let translations;
-		if (language == "en") {
-			translations = await hotel_trans_en
-				.find({ listingId: { $in: hotelsId } })
-				.toArray();
-		} else if (language == "bn") {
-			translations = await hotel_trans_bn
-				.find({ listingId: { $in: hotelsId } })
-				.toArray();
-		}
-
-		// 3. Merge hotels with translations
-		const merged = listings.map((listing) => {
-			const translation = translations.find((t) => t.listingId === listing._id);
-			return {
-				...listing,
-				title: translation ? translation.title : listing.title,
-				description: translation ? translation.description : listing.description,
-			};
-		});
-		res.json(merged);
-	} catch (err) {
-		console.error(err);
-		res.status(500).json({ message: "Internal server error" });
-	}
-});
-
-//data for experience page
-
-app.get("/services", async (req, res) => {
-	try {
-		const { category, language, limit = 8 } = req.query;
-
-		if (!category || !language) {
-			return res
-				.status(400)
-				.json({ message: "division and language are required" });
-		}
-
-		// 1. Get listings for   the division
-		const listings = await services.find({ category }).toArray();
-		const servicesId = listings.map((l) => l._id);
-
-		// 2. Get translations for those hotels
-		let translations;
-		if (language == "en") {
-			translations = await services_trans_en
-				.find({ listingId: { $in: servicesId } })
-				.toArray();
-		} else if (language == "bn") {
-			translations = await services_trans_bn
-				.find({ listingId: { $in: servicesId } })
-				.toArray();
-		}
-
-		// 3. Merge hotels with translations
-		const merged = listings.map((listing) => {
-			const translation = translations.find((t) => t.listingId === listing._id);
-			return {
-				...listing,
-				title: translation ? translation.title : listing.title,
-				description: translation ? translation.description : listing.description,
-			};
-		});
-
-		// 4. Shuffle merged array
-		for (let i = merged.length - 1; i > 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1));
-			[merged[i], merged[j]] = [merged[j], merged[i]];
-		}
-		res.json(merged);
-	} catch (err) {
-		console.error(err);
-		res.status(500).json({ message: "Internal server error" });
-	}
-});
-app.get("/experiences", async (req, res) => {
-	try {
-		const { division, language, limit = 8 } = req.query;
-
-		if (!division || !language) {
-			return res
-				.status(400)
-				.json({ message: "division and language are required" });
-		}
-
-		// 1. Get listings for   the division
-		const listings = await experiences.find({ division }).toArray();
-
-		const experiencesId = listings.map((l) => l._id);
-
-		// 2. Get translations for those hotels
-		let translations;
-		if (language == "en") {
-			translations = await experiences_trans_en
-				.find({ listingId: { $in: experiencesId } })
-				.toArray();
-		} else if (language == "bn") {
-			translations = await experiences_trans_bn
-				.find({ listingId: { $in: experiencesId } })
-				.toArray();
-		}
-
-		// 3. Merge hotels with translations
-		const merged = listings.map((listing) => {
-			const translation = translations.find((t) => t.listingId === listing._id);
-			return {
-				...listing,
-				title: translation ? translation.title : listing.title,
-				description: translation ? translation.description : listing.description,
-			};
-		});
-
-		// 4. Shuffle merged array
-		for (let i = merged.length - 1; i > 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1));
-			[merged[i], merged[j]] = [merged[j], merged[i]];
-		}
-		res.json(merged);
-	} catch (err) {
-		console.error(err);
-		res.status(500).json({ message: "Internal server error" });
-	}
-});
 
 // Start server
 // app.listen(PORT, () => {
